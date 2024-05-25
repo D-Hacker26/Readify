17678266509cb48d2aa6b325eb6f32d55a898366
D-Hacker26 <iec2022006@iiita.ac.in>
2024-05-26 00:54:36 +0530

    First commit

---
+++ app/src/main/java/com/example/readify/splashs1.kt
@@ -0,0 +1,11 @@
+package com.example.readify
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class splashs1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splashs1)
+    }
+}
