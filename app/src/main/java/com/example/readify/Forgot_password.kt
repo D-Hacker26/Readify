14a50436fc4cbf45f2a0b3da9f5e8122cb4a1fb0
D-Hacker26 <iec2022006@iiita.ac.in>
2024-05-27 02:11:38 +0530

    Added pages

---
+++ app/src/main/java/com/example/readify/Forgot_password.kt
@@ -0,0 +1,11 @@
+package com.example.readify
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Forgot_password : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_forgot_password)
+    }
+}
