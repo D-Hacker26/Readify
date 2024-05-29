42a458b8fbceb99e3fa8233a64e83254af0d1e8d
D-Hacker26 <iec2022006@iiita.ac.in>
2024-05-29 19:15:23 +0530

    Improved UI

---
+++ app/src/main/java/com/example/readify/new_book.kt
@@ -0,0 +1,11 @@
+package com.example.readify
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class new_book : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_book)
+    }
+}
