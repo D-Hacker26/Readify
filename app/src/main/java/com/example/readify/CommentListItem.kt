cde474176dc15d9cc09f75d60bdc68c6ddc8ca68
D-Hacker26 <iec2022006@iiita.ac.in>
2024-07-04 21:52:17 +0530

    Comment section added

---
+++ app/src/main/java/com/example/readify/CommentListItem.kt
@@ -0,0 +1,11 @@
+package com.example.readify
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CommentListItem : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_comment_list_item)
+    }
+}
